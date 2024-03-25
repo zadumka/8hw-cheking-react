7e16668b423d4db37174103dfd6bd12f9850523e
zadumka <alenahas1987@gmail.com>
2024-03-25 14:15:09 +0200

    changed file loginform

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,44 +1,76 @@
-import { useDispatch } from 'react-redux';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
-import { logIn } from '../../redux/auth/operations';
+import { useId } from 'react';
 
 import css from './LoginForm.module.css';
 
-export default function LoginForm() {
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/auth/operation';
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const CheckShema = Yup.object().shape({
+  email: Yup.string().email('Pls valid email').required('Required email'),
+  password: Yup.string().min(6, 'Too short').max(50, 'Too long'),
+});
+
+export const LogInForm = () => {
+  const idEmail = useId();
+  const idPassword = useId();
+
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    )
-      .unwrap()
-      .then(() => {
-        console.log('login success');
-      })
-      .catch(() => {
-        console.log('login error');
-      });
-
-    form.reset();
+  const handleSubmit = (values, actions) => {
+    dispatch(login(values));
+    actions.resetForm();
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <>
+      <h2 className={css.title}>Log in</h2>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={CheckShema}
+      >
+        <Form className={css.form}>
+          <div className={css.field}>
+            <label htmlFor={idEmail} className={css.label}>
+              Email
+            </label>
+            <Field
+              type="text"
+              name="email"
+              id={idEmail}
+              className={css.input}
+            />
+            <ErrorMessage name="email" component="span" className={css.error} />
+          </div>
+
+          <div className={css.field}>
+            <label htmlFor={idPassword} className={css.label}>
+              Password
+            </label>
+            <Field
+              type="text"
+              name="password"
+              id={idPassword}
+              className={css.input}
+            />
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={css.error}
+            />
+          </div>
+          <button type="submit" className={css.btn}>
+            Log in
+          </button>
+        </Form>
+      </Formik>
+    </>
   );
-}
+};
