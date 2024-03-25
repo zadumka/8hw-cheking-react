5145c1c91939b16fa4318061e58a45c91854e128
zadumka <alenahas1987@gmail.com>
2024-03-25 15:19:27 +0200

    changed registrationform component

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,42 +1,90 @@
-import { useDispatch } from 'react-redux';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
-import { register } from '../../redux/auth/operations';
+import { useId } from 'react';
 
 import css from './RegistrationForm.module.css';
 
-export default function RegistrationForm() {
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { register } from '../../redux/auth/operation';
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const CheckShema = Yup.object().shape({
+  name: Yup.string()
+    .min(5, 'Too short')
+    .max(50, 'Too long')
+    .required('Required name'),
+  email: Yup.string().email('Pls valid email').required('Required email'),
+  password: Yup.string().min(6, 'Too short').max(50, 'Too long'),
+});
+
+export const RegisterForm = () => {
+  const idName = useId();
+  const idEmail = useId();
+  const idPassword = useId();
+
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-
-    form.reset();
+  const handleSubmit = (values, actions) => {
+    dispatch(register(values));
+    actions.resetForm();
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Username
-        <input type="text" name="name" />
-      </label>
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <>
+      <h2 className={css.title}>Create account</h2>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={CheckShema}
+      >
+        <Form className={css.form}>
+          <div className={css.field}>
+            <label htmlFor={idName} className={css.label}>
+              Name
+            </label>
+            <Field type="text" name="name" id={idName} className={css.input} />
+            <ErrorMessage name="name" component="span" className={css.error} />
+          </div>
+
+          <div className={css.field}>
+            <label htmlFor={idEmail} className={css.label}>
+              Email
+            </label>
+            <Field
+              type="text"
+              name="email"
+              id={idEmail}
+              className={css.input}
+            />
+            <ErrorMessage name="email" component="span" className={css.error} />
+          </div>
+
+          <div className={css.field}>
+            <label htmlFor={idPassword} className={css.label}>
+              Password
+            </label>
+            <Field
+              type="text"
+              name="password"
+              id={idPassword}
+              className={css.input}
+            />
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={css.error}
+            />
+          </div>
+          <button type="submit" className={css.btn}>
+            Sign up
+          </button>
+        </Form>
+      </Formik>
+    </>
   );
-}
+};
