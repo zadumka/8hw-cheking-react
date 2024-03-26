22880f31aec1cfb83a5965ce52ce3d9ac1d2ebbe
zadumka <alenahas1987@gmail.com>
2024-03-26 12:47:31 +0200

    changed filters/slice, selectors

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,1 +1,1 @@
-export const selectNameFilter = state => state.filters.name;
+export const selectFilter = state => state.filters.name;
--- src/redux/filters/slice.js
+++ src/redux/filters/slice.js
@@ -12,4 +12,4 @@ const filtersSlice = createSlice({
 
 export const { changeFilter } = filtersSlice.actions;
 export const filtersReducer = filtersSlice.reducer;
-// export const selectNameFilter = state => state.filters.name;
+export const selectNameFilter = state => state.filters.name;
