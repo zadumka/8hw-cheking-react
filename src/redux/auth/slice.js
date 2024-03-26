87ab34d6117fced40a6280ea0136f28acc50e412
zadumka <alenahas1987@gmail.com>
2024-03-26 13:58:31 +0200

    changed auth/slice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -3,15 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: 'login',
   initialState: {
     user: {
       name: null,
       email: null,
+      token: null,
+      isLoggedIn: false,
+      isRefreshing: false,
     },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
   },
   extraReducers: builder => {
     builder
